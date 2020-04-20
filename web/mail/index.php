f75563fce8bfb40280b87caa419bfcc14f3aaa13
OB01 <ob_1@outlook.com>
2020-04-20 18:59:13 +0100

    update

--- web/mail/index.php
+++ web/mail/index.php
@@ -7,14 +7,14 @@
    <body>
       
       <?php
-         $to = "xyz@somedomain.com";
-         $subject = "This is subject";
+         $to = "ob_1@outlook.com";
+         $subject = "Send mail response";
          
          $message = "<b>This is HTML message.</b>";
          $message .= "<h1>This is headline.</h1>";
          
-         $header = "From:abc@somedomain.com \r\n";
-         $header .= "Cc:afgh@somedomain.com \r\n";
+         $header = "From:admin@native.codes \r\n";
+         // $header .= "Cc:afgh@somedomain.com \r\n";
          $header .= "MIME-Version: 1.0\r\n";
          $header .= "Content-type: text/html\r\n";
          

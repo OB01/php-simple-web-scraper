79a040cb93347b3284efbcab2446866dddd45ec9
OB01 <ob_1@outlook.com>
2020-04-28 18:14:58 +0100

    upate

--- web/ean/index.php
+++ web/ean/index.php
@@ -1,10 +1,4 @@
 <?php 
 phpinfo();
 
-$q = $_SERVER["QUERY_STRING"] ? $_SERVER["QUERY_STRING"] : false;
-
-if ($q && strlen($q)==13 && is_numeric($q) {
-    $json = file_get_contents("https://www.batzo.net/api/v1/products?barcode=".$q."&key=137KgWcLdudxbneOrk8IkJfA5Hm9nEzedHOb");
-    echo $json;
-};
 ?> 

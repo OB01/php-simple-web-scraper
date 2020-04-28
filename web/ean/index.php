963a180d435c80ede37a767ad9cc8cebfc6ac9ae
OB01 <ob_1@outlook.com>
2020-04-28 13:23:48 +0100

    update

--- web/ean/index.php
+++ web/ean/index.php
@@ -1,17 +1,23 @@
 <?php
     $file = 'codes.ini';
     $codes = parse_ini_file($file);
-    header('Content-Type: application/json');
 
     if(isset($_GET['code']) && array_key_exists($_GET['code'], $codes)){
+        header('Content-Type: application/json');
         echo $codes[$_GET['code']];
     }
     else{
         if (isset($_GET['code']) && strlen($_GET['code'])===13) {
             $json = file_get_contents("https://www.batzo.net/api/v1/products?barcode=".$_GET['code']."&key=137KgWcLdudxbneOrk8IkJfA5Hm9nEzedHOb");
+            header('Content-Type: application/json');
             echo $json;
             $codes[$code] = $json;
             write_ini_file($codes, $file);
+            exit;
+        }
+        else{
+            header('HTTP/1.0 404 Not Found');
+            echo 'Unknown code.';
         };
     };
 ?>

4ffa64e85d5b5bb3d633cb84604710f664e99ca1
OB01 <ob_1@outlook.com>
2020-04-28 18:13:19 +0100

    update

--- web/ean/index.php
+++ web/ean/index.php
@@ -1,28 +1,9 @@
 <?php 
-header('Content-Type: application/json');
-  
-if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') 
-    $link = "https"; 
-else
-    $link = "http"; 
-  
-// Here append the common URL characters. 
-$link .= "://"; 
-  
-// Append the host(domain name, ip) to the URL. 
-$link .= $_SERVER['HTTP_HOST']; 
-  
-// Append the requested resource location to the URL 
-$link .= $_SERVER['REQUEST_URI']; 
-      
-// Print the link 
-echo $link; 
+phpinfo();
 
 $q = $_SERVER["QUERY_STRING"] ? $_SERVER["QUERY_STRING"] : false;
 
-echo 'get '.$q;
-
-if ($q && strlen($q)==13) {
+if ($q && strlen($q)==13 && is_numeric($q) {
     $json = file_get_contents("https://www.batzo.net/api/v1/products?barcode=".$q."&key=137KgWcLdudxbneOrk8IkJfA5Hm9nEzedHOb");
     echo $json;
 };

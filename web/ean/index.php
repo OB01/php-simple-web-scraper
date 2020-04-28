7eefffb5ac451d4678f37c7815d451d2df7d56a7
OB01 <ob_1@outlook.com>
2020-04-28 17:31:33 +0100

    update

--- web/ean/index.php
+++ web/ean/index.php
@@ -1,23 +1,53 @@
 <?php
-    $file = 'codes.ini';
-    $codes = parse_ini_file($file);
+if (isset($_GET['code']) && strlen($_GET['code'])==13) {
+    $json = file_get_contents("https://www.batzo.net/api/v1/products?barcode=".$_GET['code']."&key=137KgWcLdudxbneOrk8IkJfA5Hm9nEzedHOb");
+    header('Content-Type: application/json');
+    echo $json;
+};
 
-    if(isset($_GET['code']) && array_key_exists($_GET['code'], $codes)){
-        header('Content-Type: application/json');
-        echo $codes[$_GET['code']];
+try
+{
+  //open the database
+  $db = new PDO('sqlite:EANcodes.sqlite');
+
+  //create the database
+  $db->exec("CREATE TABLE EAN (Code INTEGER, Data TEXT)");    
+
+  if($json && !isset( $json['errors'] ) ){
+    //insert some data...
+    $db->exec("INSERT INTO EAN (Code, Data) VALUES ($_GET['code'], $json);");
+    //now output the data to a simple html table...
+    print "<table border=1>";
+    print "<tr><td>Code</td><td>Data</td></tr>";
+    $result = $db->query('SELECT Data FROM EAN where code='.$_GET['code']);
+    foreach($result as $row)
+    {
+    print "<tr><td>".$row['Code']."</td>";
+    print "<td>".$row['Data']."</td>";
     }
-    else{
-        if (isset($_GET['code']) && strlen($_GET['code'])==13) {
-            $json = file_get_contents("https://www.batzo.net/api/v1/products?barcode=".$_GET['code']."&key=137KgWcLdudxbneOrk8IkJfA5Hm9nEzedHOb");
-            header('Content-Type: application/json');
-            echo $json;
-            $codes[$code] = $json;
-            write_ini_file($codes, $file);
-            exit;
-        }
-        else{
-            header('HTTP/1.0 404 Not Found');
-            echo 'Unknown code: '.$_GET['code'];
-        };
-    };
+    print "</table>";
+  }
+  else{
+  //now output the data to a simple html table...
+  print "<table border=1>";
+  print "<tr><td>Code</td><td>Data</td></tr>";
+  $result = $db->query('SELECT * EAN');
+  foreach($result as $row)
+  {
+    print "<tr><td>".$row['Code']."</td>";
+    print "<td>".$row['Data']."</td>";
+  }
+  print "</table>";
+  }
+
+
+
+  // close the database connection
+  $db = NULL;
+}
+catch(PDOException $e)
+{
+  print 'Exception : '.$e->getMessage();
+}
+
 ?>

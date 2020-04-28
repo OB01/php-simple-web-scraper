eb1d6ef22b8b07f0e31c8662df9975c7f74b5e37
OB01 <ob_1@outlook.com>
2020-04-28 18:27:40 +0100

    update

--- web/ean/index.php
+++ web/ean/index.php
@@ -9,35 +9,39 @@ if ($q && strlen($q)==13 && is_numeric($q) {
     echo $json;
 };
 
-try
-{
-    //open the database
-    $db = new PDO('sqlite:EANcodes.sqlite');
 
-    //create the database
-    $db->exec("CREATE TABLE EAN (Code INTEGER PRIMARY KEY, Data TEXT)");    
-
-    if($json){
-    //insert some data...
-    $db->exec("INSERT INTO EAN (Code, Data) VALUES ($q, $json);");
-    };
-
-    //now output the data to a simple html table...
-    print "<table border=1>";
-    print "<tr><td>Code</td><td>Data</td></tr>";
-    $result = $db->query('SELECT * FROM EAN');
-    foreach($result as $row)
+    try
     {
-    print "<tr><td>".$row['Code']."</td>";
-    print "<td>".$row['Data']."</td></tr>";
+      //open the database
+      $db = new PDO('sqlite:dogsDb_PDO.sqlite');
+  
+      //create the database
+      $db->exec("CREATE TABLE Dogs (Id INTEGER PRIMARY KEY, Breed TEXT, Name TEXT, Age INTEGER)");    
+  
+      //insert some data...
+      $db->exec("INSERT INTO Dogs (Breed, Name, Age) VALUES ('Labrador', 'Tank', 2);".
+                 "INSERT INTO Dogs (Breed, Name, Age) VALUES ('Husky', 'Glacier', 7); " .
+                 "INSERT INTO Dogs (Breed, Name, Age) VALUES ('Golden-Doodle', 'Ellie', 4);");
+  
+      //now output the data to a simple html table...
+      print "<table border=1>";
+      print "<tr><td>Id</td><td>Breed</td><td>Name</td><td>Age</td></tr>";
+      $result = $db->query('SELECT * FROM Dogs');
+      foreach($result as $row)
+      {
+        print "<tr><td>".$row['Id']."</td>";
+        print "<td>".$row['Breed']."</td>";
+        print "<td>".$row['Name']."</td>";
+        print "<td>".$row['Age']."</td></tr>";
+      }
+      print "</table>";
+  
+      // close the database connection
+      $db = NULL;
     }
-    print "</table>";
+    catch(PDOException $e)
+    {
+      print 'Exception : '.$e->getMessage();
+    }
 
-    // close the database connection
-    $db = NULL;
-}
-catch(PDOException $e)
-{
-    print 'Exception : '.$e->getMessage();
-}
 ?> 

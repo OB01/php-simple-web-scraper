23a5830d0c1731611097219da99d0bb3e58602f1
OB01 <ob_1@outlook.com>
2020-04-28 19:04:09 +0100

    update

--- web/ean/index.php
+++ web/ean/index.php
@@ -3,10 +3,13 @@ header('Content-Type: application/json');
 
 $q = $_SERVER["QUERY_STRING"] ? $_SERVER["QUERY_STRING"] : false;
 
-if (is_numeric($q) && strlen($q)==13 {
+if ($q && strlen($q)==13 && is_numeric($q)) {
     if (!file_exists($q.".json")) {  
+        $file = fopen($q.".json", "w") or die("Unable to open file!");
         $json = file_get_contents("https://www.batzo.net/api/v1/products?barcode=".$q."&key=137KgWcLdudxbneOrk8IkJfA5Hm9nEzedHOb");
         echo $json;
+        fwrite($file, $json);
+        fclose($file);
     }
     else{
         echo file_get_contents($q.".json");

dbef2afd8ea1c3a9a2807e0d1942d2aada62875a
OB01 <ob_1@outlook.com>
2020-04-28 13:09:10 +0100

    update

--- web/ean/index.php
+++ web/ean/index.php
@@ -3,16 +3,15 @@
     $codes = parse_ini_file($file);
     header('Content-Type: application/json');
 
-    if(isset($_GET[]) && array_key_exists($_GET[], $codes)){
-        echo $codes[$_GET[]];
+    if(isset($_GET['code']) && array_key_exists($_GET['code'], $codes)){
+        echo $codes[$_GET['code']];
     }
     else{
-        if (isset($_GET[]) && strlen($_GET[])===13) {
-            $json = file_get_contents("https://www.batzo.net/api/v1/products?barcode=".$_GET[]."&key=137KgWcLdudxbneOrk8IkJfA5Hm9nEzedHOb");
+        if (isset($_GET['code']) && strlen($_GET['code'])===13) {
+            $json = file_get_contents("https://www.batzo.net/api/v1/products?barcode=".$_GET['code']."&key=137KgWcLdudxbneOrk8IkJfA5Hm9nEzedHOb");
             echo $json;
             $codes[$code] = $json;
             write_ini_file($codes, $file);
-            exit;
         };
     };
 ?>

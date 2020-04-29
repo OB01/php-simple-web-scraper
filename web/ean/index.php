649942a4b1693acc549335cc964d941436ed5c27
OB01 <ob_1@outlook.com>
2020-04-29 15:15:18 +0100

    update

--- web/ean/index.php
+++ web/ean/index.php
@@ -1,24 +1,12 @@
 <?php 
 header('Content-Type: application/json');
 
-$q = $_SERVER["QUERY_STRING"] ? $_SERVER["QUERY_STRING"] : '{"errors":"Not Found"}';
+$q = $_SERVER["QUERY_STRING"] ? $_SERVER["QUERY_STRING"] : 'no query';
 
 if ($q && strlen($q)==13 && is_numeric($q)) {
-    if (!file_exists($q.".json")) {  
-        $file = fopen($q.".json", "w") or die("Unable to open file!");
-        $json = file_get_contents("https://www.batzo.net/api/v1/products?barcode=".$q."&key=137KgWcLdudxbneOrk8IkJfA5Hm9nEzedHOb");
-        echo $json;
-        fwrite($file, $json);
-        fclose($file);
-    }
-    else{
-        echo file_get_contents($q.".json");
-    };
-}
-else{
+    echo file_get_contents("https://www.batzo.net/api/v1/products?barcode=".$q."&key=137KgWcLdudxbneOrk8IkJfA5Hm9nEzedHOb");
+}else{
     echo $q;
 };
-
-
 
 ?> 
